2fca8e8a795e01a4d60d1c27cc7358c3c7964081
Soham <jambhwadekarsoham@gmail.com>
2025-03-26 23:43:49 +0530

    starting with canvas

---
+++ apps/excalidraw-frontend/app/canvas/[roomId]/page.tsx
@@ -0,0 +1,28 @@
+"use client"
+
+import { useEffect, useRef } from "react"
+
+export default function Canvas(){
+
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+
+    useEffect(() => {
+
+        if(canvasRef.current){
+            const canvas = canvasRef.current;
+            const ctx = canvas.getContext("2d");
+
+            if(!ctx){
+                return;
+            }
+
+            ctx.strokeRect(25,25,100,100);
+        }
+    } , [canvasRef]);
+
+    return(
+        <div>
+            <canvas ref={canvasRef} width={500} height={500}></canvas>
+        </div>
+    )
+}
