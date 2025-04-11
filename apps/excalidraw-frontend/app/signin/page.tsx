547ff5cff3f0bb63c31416146869e46dc346ed71
Soham <jambhwadekarsoham@gmail.com>
2025-04-11 23:32:43 +0530

    Update page.tsx

--- apps/excalidraw-frontend/app/signin/page.tsx
+++ apps/excalidraw-frontend/app/signin/page.tsx
@@ -1,5 +1,15 @@
-import AuthPage from "@/components/AuthPage";
+import AuthLayout from "@/components/auth/AuthLayout";
+import SignInForm from "@/components/auth/SginInForm";
 
-export default function Signin() {
-    return <AuthPage isSignin={true} />
-}
+const SignIn = () => {
+    return (
+      <AuthLayout
+        title="Welcome back" 
+        description="Sign in to your account to continue drawing"
+      >
+        <SignInForm />
+      </AuthLayout>
+    );
+  };
+  
+  export default SignIn;
